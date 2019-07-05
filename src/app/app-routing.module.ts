863d2c44f470d9d18504f332303cb5b26ffa2910
yusufrizalh <yusufrizalh@gmail.com>
2019-07-05 15:44:27 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+// const routes: Routes = [
+//   { path: '', redirectTo: 'home', pathMatch: 'full' },
+//   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+//   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+//   { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+//   { path: 'first-with-tabs', loadChildren: './pages/first-with-tabs/first-with-tabs.module#FirstWithTabsPageModule' },
+//   { path: 'second', loadChildren: './pages/second/second.module#SecondPageModule' },
+//   { path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+//   { path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' },
+//   { path: 'details', loadChildren: './pages/details/details.module#DetailsPageModule' },
+// ];
+
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' }
 ];
 
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+//   ],
+//   exports: [RouterModule]
+// })
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router, RouterEvent } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  selectedPath = '';
+
+  pages = [
+    {
+      title: 'First Page with Tabs',
+      url: '/menu/first'
+    },
+    {
+      title: 'Second Page blank',
+      url: '/menu/second'
+    }
+  ];
+
+  // constructor() { }
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event && event.url) {
+        this.selectedPath = event.url;
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
