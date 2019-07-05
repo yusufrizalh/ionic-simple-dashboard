52d41010e1a48fc4b3008ce665d08464f3b111f4
yusufrizalh <yusufrizalh@gmail.com>
2019-07-05 16:51:38 +0700

    add firebase service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {   // buat credentials firebase disini
+    apiKey: "AIzaSyCrRc5qEZp7fajOhlZ5ntwXygx7BflJXdI",
+    authDomain: "event-manager-ddaf4.firebaseapp.com",
+    databaseURL: "https://event-manager-ddaf4.firebaseio.com",
+    projectId: "event-manager-ddaf4",
+    storageBucket: "event-manager-ddaf4.appspot.com",
+    messagingSenderId: "870626382102",
+    appId: "1:870626382102:web:aa6d086cbfad1abf"
+  }
 };
 
 /*
